c0270c140030536e3f115762ac70069c9313ebeb
Ebrahim <ebi@nejati.net>
2022-08-22 01:03:41 +0430

    Init echo bot with gotd

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+
+	"github.com/gotd/td/telegram"
+	"github.com/gotd/td/telegram/message"
+	"github.com/gotd/td/tg"
+)
+
+func main() {
+	c := GenConfig()
+
+	ctx := context.Background()
+
+	dispatcher := tg.NewUpdateDispatcher()
+	opts := telegram.Options{
+		Logger:        &c.Logger,
+		UpdateHandler: dispatcher,
+	}
+
+	client := telegram.NewClient(c.AppId, c.AppHash, opts)
+
+	if err := client.Run(ctx, func(ctx context.Context) error {
+		if _, err := client.Auth().Bot(ctx, c.BotToken); err != nil {
+			return err
+		}
+
+		api := tg.NewClient(client)
+		sender := message.NewSender(api)
+		dispatcher.OnNewMessage(func(ctx context.Context, entities tg.Entities, u *tg.UpdateNewMessage) error {
+			m, ok := u.Message.(*tg.Message)
+			if !ok || m.Out {
+				return nil
+			}
+
+			_, err := sender.Reply(entities, u).Text(ctx, m.Message)
+			return err
+		})
+
+		if err := telegram.RunUntilCanceled(ctx, client); err != nil {
+			c.Logger.Error("Unable for client to run until canceled")
+		}
+		return nil
+	}); err != nil {
+		panic(err)
+	}
+}
